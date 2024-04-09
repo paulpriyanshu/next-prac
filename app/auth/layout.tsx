c3b1e079180777fea7b82b500a85e2473b219069
paulpriyanshu <priyanshu.paul003@gmail.com>
2024-04-09 19:15:32 +0530

    some changes

---
+++ app/auth/layout.tsx
@@ -0,0 +1,10 @@
+export default function titlebar({children}:{
+    children:React.ReactNode
+}){
+    return <>
+        <div className="border-b text-center">
+            hello
+        </div>
+        {children}
+    </>
+}
---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function signup(){
+
+    return  <>
+        <div className="text-3xl text-center bg-slate-400">
+            signup here
+
+        </div>
+    </>
+
+}
